aeb3d3252395ee1cb4e435bbbe5bccda324a6e4c
Vx-tech <xaviervianney8@gmail.com>
2026-01-12 08:21:22 +0300

    Setting up the listing 04 application

---
+++ interfaces/index.ts
@@ -0,0 +1,23 @@
+import React from "react";
+export interface PropertyProps {
+  name: string;
+
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+
+  rating: number;
+  category: string[];
+  price: number;
+
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+
+  image: string;
+  discount: string;
+}
